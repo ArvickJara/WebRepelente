4402772e05e14189abbbc2b2329224d66d5f09b4
Arvic Kenedi <arvic2514@gmail.com>
2024-10-02 03:40:36 -0500

    primera version

---
+++ script.js
@@ -0,0 +1,6 @@
+// Este archivo puede manejar cualquier interacción futura con la web.
+// Actualmente solo gestiona la visualización de los modales en la página.
+document.addEventListener('DOMContentLoaded', function () {
+    var modalEl = document.getElementById('historiaModal');
+    var myModal = new bootstrap.Modal(modalEl);
+});
